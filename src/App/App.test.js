7586f44183a3887c9214e7e7ce0ac4e325ef79d2
Kelli Watkins <watkinskellil@gmail.com>
2023-05-30 12:20:21 -0600

    test: start cypress testing

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,6 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+describe('Home Page', () => {
+  it('should display existing reservations', () => {
+    beforeEach(() => {
+      cy.intercept('GET', 'http://localhost:3001/api/v1/reservations', {
+        statusCode: 200,
+        body: {
+          [
+            {
+              "id": 1,
+              "name": "Christie",
+              "date": "12/29",
+              "time": "7:00",
+              "number": 12
+            },
+          ]           
+        },
+      }).as('existingReservation');
+
+      cy.visit('localhost:3000')
+    })
+  })
+})
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
